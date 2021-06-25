9daec875fcad393a40c025b09c44202afe0f5b7d
Oliver <oliver.mwitia@gmail.com>
2021-06-26 00:13:11 +0300

    chore: Update some android deps and use material theme

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -31,7 +31,7 @@ android {
 
     kotlinOptions {
         jvmTarget = "1.8"
-     }
+    }
 
     buildFeatures {
         compose = true
@@ -44,13 +44,16 @@ android {
 
 dependencies {
     implementation(project(":common"))
+
     implementation("com.google.android.material:material:1.3.0")
-    implementation("androidx.appcompat:appcompat:1.2.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+
+    implementation("androidx.activity:activity-compose:1.3.0-beta02")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-beta01")
+
+
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${rootProject.extra["compose_version"]}")
 }
