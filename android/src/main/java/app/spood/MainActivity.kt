fcd6670c2779a68c2d94ab4c3aaa93fadfc4966b
Oliver <oliver.mwitia@gmail.com>
2021-06-25 23:28:30 +0300

    feat: Add SplashScreen on android

--- android/src/main/java/app/spood/MainActivity.kt
+++ android/src/main/java/app/spood/MainActivity.kt
@@ -3,39 +3,23 @@ package app.spood
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.Scaffold
 import app.spood.ui.theme.SpoodTheme
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SpoodTheme {
-                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting(greet())
-                }
+                Scaffold(
+                    content = {SplashScreen(greet())}
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(message: String) {
-    Text(text = message)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SpoodTheme {
-        Greeting(greet())
+    private fun greet(): String {
+        return Greeting().greeting()
     }
 }
---
+++ android/src/main/java/app/spood/SplashScreen.kt
@@ -0,0 +1,67 @@
+package app.spood
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import app.spood.ui.theme.white
+import app.spood.ui.theme.yellow
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen("Hello ")
+}
+
+@Composable
+fun SplashScreen(message: String) {
+    val goToAuthenticate: () -> Unit = {
+        Log.e("qwertyu", "ertyumo")
+    }
+
+    Scaffold(
+        backgroundColor = yellow,
+        content = { Content(goToAuthenticate) },
+        floatingActionButton = { FloatingButton(goToAuthenticate) }
+    )
+}
+
+@Composable
+fun Content(goToAuthenticate: () -> Unit, modifier: Modifier = Modifier) {
+    val logo = painterResource(id = R.drawable.logo)
+    val banner = painterResource(id = R.drawable.banner)
+    val tagline = painterResource(id = R.drawable.tagline)
+    Column(
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
+        content = {
+            Image(
+                painter = logo,
+                contentDescription = "Logo",
+                modifier = Modifier.clickable(onClick = goToAuthenticate)
+            )
+            Image(painter = banner, contentDescription = "Banner")
+            Image(painter = tagline, contentDescription = "Tag line")
+        }
+    )
+}
+
+@Composable
+fun FloatingButton(goToAuthenticate: () -> Unit) {
+    val arrowRight = painterResource(id = R.drawable.ic_arrow_right)
+    FloatingActionButton(
+        backgroundColor = white,
+        onClick = goToAuthenticate,
+        content = { Image(painter = arrowRight, contentDescription = "Arrow Right") }
+    )
+}
